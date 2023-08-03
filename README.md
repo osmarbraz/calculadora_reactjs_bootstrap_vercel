e77ae9d72ff5348e410634f3e93f9f324d9db112
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-08-03 11:00:02 -0300

    Início do projeto.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Programa cliente da API Calculadora utilizando React.js com Bootstrap no Vercel.
+
+- O projeto foi desenvolvido no VSCode deve ser chamado "calculadora_reactjs_bootstrap_vercel".
+- Programa servidor está no projeto "calculadora_nodejs_vercel".
+- Programa cliente web utiliza a porta 3000.
+
+- Dependências:    
+    - bootstrap,
+    - react-bootstrap.
+
+- Execução:    
+   <pre><code>npm start</code></pre>
+
+- Arquivos em src:
+   - App.js - Interface do programa.
+
+- Vercel
+   https://calculadora_reactjs_bootstrap_vercel.vercel.app/      
---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import './App.css';
+import { Container, Form, Button } from 'react-bootstrap';
+
+function FrmCalculadora() {
+ 
+    // Estados inciais das variáveis do formulário
+    const [valorA, setValorA] = useState(0);
+    const [operacao, setOperacao] = useState('adicao');
+    const [valorB, setValorB] = useState(0);
+    const [resultado, setResultado] = useState(0);
+  
+    // Submissão do formulário.
+    const handleSubmit = (event) => {
+      // Impede o recarregamento da página
+      event.preventDefault();      
+      //Endereço da API + operação + valorA + valorB
+      fetch(`calculadora-nodejs-vercel.vercel.app/${operacao}/${valorA}/${valorB}`)
+        .then((res) => res.json()) //Converte a resposta para JSON
+        .then((data) => setResultado(data.resultado)); // Atribui a resposta ao resultado
+    };
+
+    // Limpa os campos do formulário.     
+    const limpar = () => { 
+      setValorA(0);
+      setOperacao('adicao');
+      setValorB(0);
+      setResultado(0);
+    };
+  
+    // Renderiza o formulário
+    return (     
+      <>
+      <Container> 
+        <Form name="FrmCalculadora" method="get" onSubmit={handleSubmit}>
+          <Form.Label><h1>Formulário Calculadora</h1> </Form.Label><br/>
+          <Form.Group>
+            <Form.Label>Valor A: 
+            <Form.Control type="number" name="valorA" value={valorA} onChange={(event) => setValorA(event.target.value)}/>
+            </Form.Label><br/>
+          </Form.Group>     
+          <Form.Group>
+            <Form.Label>Opera&ccedil;&atilde;o:</Form.Label>
+            <Form.Select name="operacao" value={operacao} onChange={(event) => setOperacao(event.target.value)} >
+                        <option value="adicao">Adi&ccedil;&atilde;o</option>
+                        <option value="subtracao">Subtra&ccedil;&atilde;o</option>
+                        <option value="multiplicacao">Multiplica&ccedil;&atilde;o</option>
+                        <option value="divisao">Divis&atilde;o</option>
+            </Form.Select><br/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Valor B: 
+            <Form.Control type="number" id="valorB" name="valorB" value={valorB} onChange={(event) => setValorB(event.target.value)} /></Form.Label><br/>
+          </Form.Group>
+          <Form.Group>
+            <Button variant="secondary" name="Limpar" onClick={limpar}>Limpar</Button>
+            <Button variant="primary" type="submit" name="Calcular">Calcular</Button>
+          </Form.Group>
+          <Form.Group>
+            <br/><Form.Label>Resultado: {resultado} </Form.Label>     
+          </Form.Group>
+        </Form>
+      </Container>
+      </>
+    )
+  };
+  
+  export default FrmCalculadora;
